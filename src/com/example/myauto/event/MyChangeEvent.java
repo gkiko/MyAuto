fc0996c88896b46ff8decf685af2cd4c3aaf205f
gkiko <gkiko10@freeuni.edu.ge>
2013-06-05 09:35:59 +0400

    events gadacemuli obieqtis shenaxva sheudzlia

--- src/com/example/myauto/event/MyChangeEvent.java
+++ src/com/example/myauto/event/MyChangeEvent.java
@@ -5,9 +5,11 @@ import java.util.EventObject;
 public class MyChangeEvent extends EventObject{
 
 	private static final long serialVersionUID = 1L;
-
+	public Object source;
+	
 	public MyChangeEvent(Object source) {
 		super(source);
+		this.source = source;
 	}
 
 }
