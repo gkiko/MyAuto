d54a13cf295f5fd15a376f68f723a1f46031d60e
Giorgi Kikolashvili <gkiko10@freeuni.edu.ge>
2013-06-16 15:24:33 +0400

    patara bug gasworda. siis yvela manqanas erti saxeli qonda

--- src/com/example/myauto/item/CarItem.java
+++ src/com/example/myauto/item/CarItem.java
@@ -1,9 +1,11 @@
 package com.example.myauto.item;
 
+import java.util.HashMap;
 import java.util.Observable;
 
 public class CarItem extends Observable implements Item{
-
+	private HashMap<String, String> itemData = new HashMap<String, String>();
+	
 	@Override
 	public void setValueToProperty(String property, String value) {
 		itemData.put(property, value);
--- src/com/example/myauto/item/Item.java
+++ src/com/example/myauto/item/Item.java
@@ -1,10 +1,7 @@
 package com.example.myauto.item;
 
-import java.util.HashMap;
 
 public interface Item {
-	static HashMap<String, String> itemData = new HashMap<String, String>();
-	
 	public void setValueToProperty(String property, String value);
 	
 	public String getValueFromProperty(String property);
