24ef8e84eae70c67f55066c8e8959127b09fcdf9
mda <jilbertaz@yahoo.com>
2013-06-17 02:21:24 +0400

    Catalog Page-shi Menu-s damateba da implementacia

--- src/com/example/myauto/CatalogPageActivity.java
+++ src/com/example/myauto/CatalogPageActivity.java
@@ -2,9 +2,16 @@ package com.example.myauto;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class CatalogPageActivity extends Activity {
+	
+	public static final int M_I_ID_CAR_LIST = 1;
+	public static final int M_I_ID_SEARCH = 2;
+	public static final int M_I_ID_CATALOG = 3;
+	public static final int M_I_ID_ABOUT = 4;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -12,11 +19,43 @@ public class CatalogPageActivity extends Activity {
 		setContentView(R.layout.activity_catalog_page);
 	}
 
+	/**
+	 * Menu Itemebis Damateba
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
+		menu.add(0, M_I_ID_CAR_LIST, 1, R.string.menu_main);
+		menu.add(0, M_I_ID_SEARCH, 1, R.string.menu_search);
+		menu.add(0, M_I_ID_CATALOG, 1, R.string.menu_catalog);
+		menu.add(0, M_I_ID_ABOUT, 1, R.string.menu_about);
+		
 		getMenuInflater().inflate(R.menu.catalog_page, menu);
 		return true;
+	}
+	
+	/**
+	 * Menu Itemebis Implementacia
+	 */
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		Intent nextIntent = null;
+		switch(item.getItemId()){
+		case M_I_ID_CAR_LIST:
+			nextIntent = new Intent(CatalogPageActivity.this, MainActivity.class);
+			break;
+		case M_I_ID_SEARCH:
+			nextIntent = new Intent(CatalogPageActivity.this, SearchPageActivity.class);
+			break;
+		case M_I_ID_CATALOG:
+			nextIntent = new Intent(CatalogPageActivity.this, CatalogPageActivity.class);
+			break;
+		case M_I_ID_ABOUT:
+			nextIntent = new Intent(CatalogPageActivity.this, AboutPageActivity.class);
+		default:
+			break;
+		}
+		startActivity(nextIntent);
+		return super.onMenuItemSelected(featureId, item);
 	}
 
 }
