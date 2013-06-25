8c4c70c24a5426020909e5176f5832814a1507a7
Kote Ujmajuridze <kujma10@freeuni.edu.ge>
2013-06-25 12:52:19 +0400

    login dialog
    დაემატა ლოგინის ალერტბოქსი(მხოლოდ პირველ გვერდზე!). რათქმაუნდა ჯერ არ
    მუშაობს, რადგან არ გვაქვს აპი.

--- src/com/example/myauto/FirstPageActivity.java
+++ src/com/example/myauto/FirstPageActivity.java
@@ -11,8 +11,11 @@ import com.example.myauto.item.CarFacade;
 import com.example.myauto.listener.CallbackListener;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -121,11 +124,45 @@ public class FirstPageActivity extends Activity implements CallbackListener{
 			break;
 		case R.id.menu_about:
 			nextIntent = new Intent(FirstPageActivity.this, AboutPageActivity.class);
+			break;
+		case R.id.menu_login:
+			createLoginBox();
+			return super.onMenuItemSelected(featureId, item);
 		default:
 			break;
 		}
 		startActivity(nextIntent);
 		return super.onMenuItemSelected(featureId, item);
+	}
+
+	/**
+	 * აგდებს ლოგინის დიალოგბოქსს, სადაც მომხმარებელს შეეძლება დალოგინება.
+	 *
+	 */
+	private void createLoginBox() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setCancelable(true);
+		//builder.setIcon(R.drawable.dialog_question);
+		builder.setTitle(getString(R.string.login_dialog_title));
+		builder.setInverseBackgroundForced(true);
+		LayoutInflater inflater = getLayoutInflater();
+		View dialoglayout = inflater.inflate(R.layout.login_dialog_layout, null);
+		builder.setView(dialoglayout);
+		builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+		  @Override
+		  public void onClick(DialogInterface dialog, int which) {
+		    dialog.dismiss();
+		  }
+		});
+		builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+		  @Override
+		  public void onClick(DialogInterface dialog, int which) {
+		    dialog.dismiss();
+		  }
+		});
+		AlertDialog alert = builder.create();
+		alert.show();
+		
 	}
 
 	@Override
