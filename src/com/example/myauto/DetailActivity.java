b0fd2a0bcd309fdf64da7ef1a1ec2d86926ab412
mda <jilbertaz@yahoo.com>
2013-06-30 20:59:47 +0400

    Detailed Page

--- src/com/example/myauto/DetailActivity.java
+++ src/com/example/myauto/DetailActivity.java
@@ -1,26 +1,46 @@
 package com.example.myauto;
 
+import java.util.ArrayList;
+
+import com.example.myauto.item.Item;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.WindowManager;
+import android.webkit.WebSettings.ZoomDensity;
 import android.webkit.WebView;
 import android.support.v4.app.NavUtils;
 import android.annotation.TargetApi;
+import android.content.Context;
 import android.os.Build;
 
 public class DetailActivity extends MasterPageActivity {
+	
+	private static final String CAR_DETAILS_URL = "http://www.myauto.ge/car_details.php?car_id=";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_detail);
-		// Show the Up button in the action bar.
-	//	setupActionBar();
 		
-		WebView web = (WebView) findViewById(R.id.detailed_webview);
-		web.getSettings().setJavaScriptEnabled(false);
-		web.loadUrl("http://www.myauto.ge/car_details.php?car_id=6524161");
+		Bundle bundle = getIntent().getExtras();
+		Item itm = (Item) bundle.get(FirstPageActivity.bundleKey);
+		
+		String carID = itm.getValueFromProperty("id");
+		String url = CAR_DETAILS_URL + carID;
+		
+		WebView webView = (WebView) findViewById(R.id.detailed_webview);
+		webView.getSettings().setJavaScriptEnabled(true);
+		webView.getSettings().setLoadWithOverviewMode(true);
+		webView.getSettings().setUseWideViewPort(true);
+		webView.getSettings().setBuiltInZoomControls(true);
+		webView.getSettings().setDefaultZoom(ZoomDensity.FAR);
+		webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+		webView.setScrollbarFadingEnabled(false);
+		webView.loadUrl(url);
 	}
 
 	/**
