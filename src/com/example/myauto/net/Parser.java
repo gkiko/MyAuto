0b1e9beaf7c67e2e323e79cd9b2b288f0fbd21d0
Giorgi Kikolashvili <gkiko10@freeuni.edu.ge>
2013-06-30 14:58:02 +0400

    parseri ganavarotda

--- src/com/example/myauto/net/Parser.java
+++ src/com/example/myauto/net/Parser.java
@@ -34,99 +34,52 @@ public class Parser {
 					"hydraulics", "chairWarming", "obsIndicator", "turbo",
 					"clientName", "phone", "wheel", "engine", "fuel", "gear",
 					"location", "desc", "dealer");
-	private static final boolean[] tagStauses = new boolean[tagNames.size()];
-
-	private final String idTag = "id";
 	private final String photoTag = "photo";
-	private final String priceTag = "price";
-	private final String manufacturerTag = "manufacturer";
-	private final String modelTag = "model";
-	private final String yearTag = "year";
-
+	
 	public Parser() {
 		SAXParserFactory factory = SAXParserFactory.newInstance();
 		try {
 			saxParser = factory.newSAXParser();
 			handler = new DefaultHandler() {
 				CarFacade cf;
-
-				boolean tId = false;
-				boolean tPhoto = false;
-				boolean tPrice = false;
-				boolean tMake = false;
-				boolean tModel = false;
-				boolean tYear = false;
+				String value;
 
 				public void startElement(String uri, String localName,
 						String qName, Attributes attributes)
 						throws SAXException {
-					if (qName.equalsIgnoreCase(idTag)) {
+					if(tagIsFirstInList(qName)){
 						cf = new CarFacade(new CarImageable(), new CarItem());
-						tId = true;
-					} else {
-						if (qName.equalsIgnoreCase(photoTag)) {
-							tPhoto = true;
-						} else {
-							if (qName.equalsIgnoreCase(priceTag)) {
-								tPrice = true;
-							} else {
-								if (qName.equalsIgnoreCase(manufacturerTag)) {
-									tMake = true;
-								} else {
-									if (qName.equalsIgnoreCase(modelTag)) {
-										tModel = true;
-									} else {
-										if (qName.equalsIgnoreCase(yearTag)) {
-											tYear = true;
-										}
-									}
-								}
-							}
-						}
 					}
 				}
 
 				public void endElement(String uri, String localName,
 						String qName) throws SAXException {
+					if(itIsTag(qName)){
+						if(tagIsFirstInList(qName)){
+							elements.add(cf);
+						}
+						cf.setValueToProperty(qName, value);
+						if(photoTag(qName)){
+							cf.setURL(value);
+						}
+					}
 				}
 
 				public void characters(char ch[], int start, int length)
 						throws SAXException {
-					String value = new String(ch, start, length);
-					setValToField(value, cf);
+					value = new String(ch, start, length);
 				}
 				
-				private void setValToField(String value, CarFacade cf){
-					if (tId) {
-						elements.add(cf);
-						cf.setValueToProperty(CarItem.ID, value);
-						tId = false;
-					} else {
-						if (tPhoto) {
-							cf.setURL(value);
-							tPhoto = false;
-						} else {
-							if (tPrice) {
-								cf.setValueToProperty(CarItem.PRICE, value);
-								tPrice = false;
-							} else {
-								if (tMake) {
-									cf.setValueToProperty(CarItem.MAKE, value);
-									tMake = false;
-								} else {
-									if (tModel) {
-										cf.setValueToProperty(CarItem.MODEL, value);
-										tModel = false;
-									} else {
-										if (tYear) {
-											cf.setValueToProperty(CarItem.YEAR, value);
-											tYear = false;
-										}
-									}
-								}
-							}
-						}
-					}
+				private boolean tagIsFirstInList(String tag) {
+					return tagNames.get(0).equals(tag);
+				}
+				
+				private boolean itIsTag(String tag) {
+					return tagNames.contains(tag);
+				}
+				
+				private boolean photoTag(String tag){
+					return tag.equalsIgnoreCase(photoTag);
 				}
 			};
 			
@@ -141,14 +94,6 @@ public class Parser {
 					return tagNames.contains(tag);
 				}
 
-				private boolean tagIsFirstInList(String tag) {
-					return tagNames.get(0).equals(tag);
-				}
-
-				private void openTag(String tag) {
-					tagStauses[tagNames.indexOf(tag)] = true;
-				}
-
 				public void endElement(String uri, String localName,
 						String qName) throws SAXException {
 					if(itIsTag(qName))
@@ -160,21 +105,6 @@ public class Parser {
 					value = new String(ch, start, length);
 				}
 				
-				private String getOpenedTag(){
-					return tagNames.get(getTagIndex());
-				}
-				
-				private int getTagIndex(){
-					for(int i=0;i<tagStauses.length;i++){
-						if(tagStauses[i])
-							return i;
-					}
-					return -1;
-				}
-				
-				private void closeTag(String tag){
-					tagStauses[tagNames.indexOf(tag)] = false;
-				}
 			};
 		} catch (Exception e) {
 			e.printStackTrace();
