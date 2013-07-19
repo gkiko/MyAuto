aeb3d0780dd0db6120d2830613be53c265e4b053
Kote Ujmajuridze <kujma10@freeuni.edu.ge>
2013-07-19 16:42:50 +0400

    loginis glexuri shemowmeba

--- src/com/example/myauto/MasterPageActivity.java
+++ src/com/example/myauto/MasterPageActivity.java
@@ -1,5 +1,9 @@
 package com.example.myauto;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.app.Activity;
@@ -13,10 +17,21 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 
 public class MasterPageActivity extends Activity {
 
 	private Menu menu;
+	private boolean logined = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -140,7 +155,8 @@ public class MasterPageActivity extends Activity {
 				.findViewById(R.id.password_edittext);
 		String userName = usrname.getText().toString();
 		String pass = pswd.getText().toString();
-		if (userName.equals("kujma10") && pass.equals("123123")) {
+		sendLoginRequest(userName, pass);
+		if (logined) {
 			MenuItem login = menu.findItem(R.id.menu_login);
 			login.setVisible(false);
 			MenuItem register = menu.findItem(R.id.menu_register);
@@ -162,6 +178,75 @@ public class MasterPageActivity extends Activity {
 		}
 
 	}
+
+	
+	private void sendLoginRequest(final String userName, final String pass) {
+		
+		Thread th = new Thread() {
+			@Override
+			public void run() {				
+				doPostR(userName, pass);
+			}
+		};
+		th.start();
+		try {
+			th.join();
+		} catch (InterruptedException exception) {
+			// TODO Auto-generated catch-block stub.
+			exception.printStackTrace();
+		}
+	}
+		
+		/**
+	 * TODO Put here a description of what this method does.
+	 *
+	 * @param userName
+	 * @param pass
+	 */
+	protected void doPostR(String userName, String pass) {
+		HttpClient httpclient = new DefaultHttpClient();
+		String cookies = "__unam=656d566-13b320bb773-3236022d-117; PHPSESSID=o11blrta66tc0p5oqeboi3m792; ad_date_2=2013-07-18; __utma=229565184.325010435.1337247288.1374057018.1374064773.87; __utmb=229565184.2.10.1374064773; __utmc=229565184; __utmz=229565184.1373798325.83.22.utmcsr=forum.ge|utmccn=(referral)|utmcmd=referral|utmcct=/";
+		HttpPost httppost = new HttpPost("http://myauto.ge/login.php");
+
+		try {
+			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+//			nameValuePairs.add(new BasicNameValuePair("ad_date_2", "2013-07-17"));
+			nameValuePairs.add(new BasicNameValuePair("action", "do_login"));
+			nameValuePairs.add(new BasicNameValuePair("password", pass));
+			nameValuePairs.add(new BasicNameValuePair("username", userName));
+			nameValuePairs.add(new BasicNameValuePair("PHPSESSID", "o11blrta66tc0p5oqeboi3m792"));
+			
+			
+			
+			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+			httppost.setHeader("Cookie", cookies);
+			httppost.setHeader("Host", "www.myauto.ge");
+			httppost.setHeader("Connection", "keep-alive");
+			httppost.setHeader("Cache-Control", "max-age=0");
+			httppost.setHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+			httppost.setHeader("Origin", "http://www.myauto.ge");
+			httppost.setHeader("Content-Type", "application/x-www-form-urlencoded");
+			httppost.setHeader("Referer", "http://www.myauto.ge/login.php");
+			httppost.setHeader("Accept-Encoding", "gzip,deflate,sdch");
+			httppost.setHeader("Accept-Language", "en-US,en;q=0.8,ru;q=0.6");
+			
+			HttpResponse response = httpclient.execute(httppost);
+			HttpEntity entity = response.getEntity();
+
+			String responseText = EntityUtils.toString(entity);
+			System.out.println("statuscode " + response.getStatusLine());
+			System.out.println("-----------------------------------------------");
+			if(responseText.contains("<a href=\"mypage.php\">" + userName + "</a>"))logined = true;
+
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+		}
+	}
+
+		
+	
 
 	/**
 	 * მომხმარებელს ინახავს სესიაში დალოგინებისას
