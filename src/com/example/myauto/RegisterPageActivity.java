d03a14b753132db861a1ae8d48216454862f1f55
Kote Ujmajuridze <kujma10@freeuni.edu.ge>
2013-07-21 14:37:09 +0400

    registraciis requesti daemata
    mosapiqrebelia rogor gavarkviot waramtebit daregistrirda tu ara

--- src/com/example/myauto/RegisterPageActivity.java
+++ src/com/example/myauto/RegisterPageActivity.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
+import com.example.myauto.requests.RegisterRequest;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -17,7 +19,6 @@ public class RegisterPageActivity extends Activity {
 
 	private Spinner years;
 
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -27,16 +28,16 @@ public class RegisterPageActivity extends Activity {
 
 	/**
 	 * TODO Put here a description of what this method does.
-	 *
+	 * 
 	 */
 	private void setYearsToSpinner() {
 		years = (Spinner) findViewById(R.id.spinnerBirthYear);
 		ArrayList<Integer> arr = new ArrayList<Integer>();
 		Date date = new Date();
-	    Calendar cal = Calendar.getInstance();
-	    cal.setTime(date);
-	    int year = cal.get(Calendar.YEAR);
-		for(int i = 1940; i <= year - 16; i++){
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(date);
+		int year = cal.get(Calendar.YEAR);
+		for (int i = 1940; i <= year - 16; i++) {
 			arr.add(i);
 		}
 		ArrayAdapter<Integer> arrayAdapter1 = new ArrayAdapter<Integer>(this,
@@ -45,39 +46,42 @@ public class RegisterPageActivity extends Activity {
 	}
 
 	public void submitForm(View v) {
-	    EditText et1 = (EditText)findViewById(R.id.reg_username);
-	    EditText et2 = (EditText)findViewById(R.id.reg_password);
-	    EditText et3 = (EditText)findViewById(R.id.reg_password2);
-	    EditText et4 = (EditText)findViewById(R.id.reg_email);
-	    EditText et5 = (EditText)findViewById(R.id.reg_name);
-	    EditText et6 = (EditText)findViewById(R.id.reg_surname);
-	    String userName = et1.getText().toString();
-	    String pass1 = et2.getText().toString();
-	    String pass2 = et3.getText().toString();
-	    String email = et4.getText().toString();
-	    String name = et5.getText().toString();
-	    String surname = et6.getText().toString();
-	    if(userName.equals("")){
-	    	Toast.makeText(getApplicationContext(), "Enter Username!",
-	    			Toast.LENGTH_LONG).show();
-	    } else if(pass1.equals("")){
-	    	Toast.makeText(getApplicationContext(), "Enter password!",
-	    			Toast.LENGTH_LONG).show();
-	    } else if(pass2.equals("")){
-	    	Toast.makeText(getApplicationContext(), "Enter password!",
-	    			Toast.LENGTH_LONG).show();
-	    } else if(email.equals("")){
-	    	Toast.makeText(getApplicationContext(), "Enter e-mail!",
-	    			Toast.LENGTH_LONG).show();
-	    } else if(!pass1.equals(pass2)){
-	    	Toast.makeText(getApplicationContext(), "Passwords are different!",
-	    			Toast.LENGTH_LONG).show();
-	    } else {
-	    	
-	    }
+		EditText et1 = (EditText) findViewById(R.id.reg_username);
+		EditText et2 = (EditText) findViewById(R.id.reg_password);
+		EditText et3 = (EditText) findViewById(R.id.reg_password2);
+		EditText et4 = (EditText) findViewById(R.id.reg_email);
+		Spinner gender = (Spinner) findViewById(R.id.spinnerGender);
+		String userName = et1.getText().toString();
+		String pass1 = et2.getText().toString();
+		String pass2 = et3.getText().toString();
+		String email = et4.getText().toString();
+
+		if (userName.equals("")) {
+			Toast.makeText(getApplicationContext(), "Enter Username!",
+					Toast.LENGTH_LONG).show();
+		} else if (pass1.equals("")) {
+			Toast.makeText(getApplicationContext(), "Enter password!",
+					Toast.LENGTH_LONG).show();
+		} else if (pass2.equals("")) {
+			Toast.makeText(getApplicationContext(), "Enter password!",
+					Toast.LENGTH_LONG).show();
+		} else if (email.equals("")) {
+			Toast.makeText(getApplicationContext(), "Enter e-mail!",
+					Toast.LENGTH_LONG).show();
+		} else if (!pass1.equals(pass2)) {
+			Toast.makeText(getApplicationContext(), "Passwords are different!",
+					Toast.LENGTH_LONG).show();
+		} else {
+			EditText et5 = (EditText) findViewById(R.id.reg_name);
+			EditText et6 = (EditText) findViewById(R.id.reg_surname);
+			String name = et5.getText().toString();
+			String surname = et6.getText().toString();
+			String[]params = new String[]{userName, pass1, name, surname, email, Long.toString(gender.getSelectedItemId()),(String) years.getSelectedItem()};
+			RegisterRequest rr = new RegisterRequest(params);
+			rr.sendRegistrationRequest();
+		}
 	}
-	
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
--- src/com/example/myauto/requests/RegisterRequest.java
+++ src/com/example/myauto/requests/RegisterRequest.java
@@ -1,5 +1,4 @@
 package com.example.myauto.requests;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -14,30 +13,23 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
-
 /**
  * TODO Put here a description of what this class does.
  *
  * @author Kote.
  *         Created Jul 21, 2013.
  */
-public class LoginRequest {
-	
-	private String userName;
-	private String password;
-	private boolean logined;
-	
-	public LoginRequest(String userName, String password){
-		this.userName = userName;
-		this.password = password;
-		logined = false;
+public class RegisterRequest {
+	String[] params;
+	public RegisterRequest(String[] params){
+		this.params = params;
 	}
 	
-	public boolean sendLoginRequest(){
+	public void sendRegistrationRequest(){
 		Thread th = new Thread() {
 			@Override
 			public void run() {				
-				doPostR();
+				register();
 			}
 		};
 		th.start();
@@ -47,27 +39,25 @@ public class LoginRequest {
 			// TODO Auto-generated catch-block stub.
 			exception.printStackTrace();
 		}
-		return logined;
 	}
 	
-	/**
-	 * TODO Put here a description of what this method does.
-	 *
-	 * @param userName
-	 * @param pass
-	 */
-	private void doPostR() {
+	private void register(){
 		HttpClient httpclient = new DefaultHttpClient();
-		String cookies = "__unam=656d566-13b320bb773-3236022d-117; PHPSESSID=o11blrta66tc0p5oqeboi3m792; ad_date_2=2013-07-18; __utma=229565184.325010435.1337247288.1374057018.1374064773.87; __utmb=229565184.2.10.1374064773; __utmc=229565184; __utmz=229565184.1373798325.83.22.utmcsr=forum.ge|utmccn=(referral)|utmcmd=referral|utmcct=/";
-		HttpPost httppost = new HttpPost("http://myauto.ge/login.php");
+		String cookies = "__unam=656d566-13e3db44333-30d82f5f-21; ad_date_2=2013-07-17; PHPSESSID=rv5fdi0a47an0u3p9fs646bg55; __utma=229565184.1943488099.1366831465.1374063221.1374077020.43; __utmb=229565184.15.10.1374077020; __utmc=229565184; __utmz=229565184.1367865217.10.5.utmcsr=google|utmccn=(organic)|utmcmd=organic|utmctr=(not%20provided)";
+		HttpPost httppost = new HttpPost("http://myauto.ge/register.php");
 
 		try {
 			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
 //			nameValuePairs.add(new BasicNameValuePair("ad_date_2", "2013-07-17"));
-			nameValuePairs.add(new BasicNameValuePair("action", "do_login"));
-			nameValuePairs.add(new BasicNameValuePair("password", password));
-			nameValuePairs.add(new BasicNameValuePair("username", userName));
-			nameValuePairs.add(new BasicNameValuePair("PHPSESSID", "o11blrta66tc0p5oqeboi3m792"));
+			nameValuePairs.add(new BasicNameValuePair("action", "do_register"));
+			nameValuePairs.add(new BasicNameValuePair("password", params[1]));
+			nameValuePairs.add(new BasicNameValuePair("username", params[0]));
+			nameValuePairs.add(new BasicNameValuePair("confirmpass", params[1]));
+			nameValuePairs.add(new BasicNameValuePair("name", params[2]));
+			nameValuePairs.add(new BasicNameValuePair("surname", params[3]));
+			nameValuePairs.add(new BasicNameValuePair("email", params[4]));
+			nameValuePairs.add(new BasicNameValuePair("gender_id", params[5]));
+			nameValuePairs.add(new BasicNameValuePair("birth_year", params[6]));
 			
 			
 			
@@ -79,7 +69,7 @@ public class LoginRequest {
 			httppost.setHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
 			httppost.setHeader("Origin", "http://www.myauto.ge");
 			httppost.setHeader("Content-Type", "application/x-www-form-urlencoded");
-			httppost.setHeader("Referer", "http://www.myauto.ge/login.php");
+			httppost.setHeader("Referer", "http://www.myauto.ge/register.php");
 			httppost.setHeader("Accept-Encoding", "gzip,deflate,sdch");
 			httppost.setHeader("Accept-Language", "en-US,en;q=0.8,ru;q=0.6");
 			
@@ -87,9 +77,9 @@ public class LoginRequest {
 			HttpEntity entity = response.getEntity();
 
 			String responseText = EntityUtils.toString(entity);
-			System.out.println("statuscode " + response.getStatusLine());
-			System.out.println("-----------------------------------------------");
-			if(responseText.contains("<a href=\"mypage.php\">" + userName + "</a>"))logined = true;
+			
+			System.out.println("code " + response.getStatusLine().getStatusCode());
+			System.out.println(responseText);
 
 		} catch (ClientProtocolException e) {
 			// TODO Auto-generated catch block
@@ -97,5 +87,4 @@ public class LoginRequest {
 			// TODO Auto-generated catch block
 		}
 	}
-	
 }
