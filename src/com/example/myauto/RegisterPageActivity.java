d6090acccdfc69fc7833db3b379665765812cad6
Kote Ujmajuridze <kujma10@freeuni.edu.ge>
2013-07-21 13:52:05 +0400

    wlebis spineris shevseba da validaciebi

--- src/com/example/myauto/RegisterPageActivity.java
+++ src/com/example/myauto/RegisterPageActivity.java
@@ -1,17 +1,83 @@
 package com.example.myauto;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class RegisterPageActivity extends Activity {
+
+	private Spinner years;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_register_page);
+		setYearsToSpinner();
 	}
 
+	/**
+	 * TODO Put here a description of what this method does.
+	 *
+	 */
+	private void setYearsToSpinner() {
+		years = (Spinner) findViewById(R.id.spinnerBirthYear);
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		Date date = new Date();
+	    Calendar cal = Calendar.getInstance();
+	    cal.setTime(date);
+	    int year = cal.get(Calendar.YEAR);
+		for(int i = 1940; i <= year - 16; i++){
+			arr.add(i);
+		}
+		ArrayAdapter<Integer> arrayAdapter1 = new ArrayAdapter<Integer>(this,
+				android.R.layout.simple_spinner_dropdown_item, arr);
+		years.setAdapter(arrayAdapter1);
+	}
+
+	public void submitForm(View v) {
+	    EditText et1 = (EditText)findViewById(R.id.reg_username);
+	    EditText et2 = (EditText)findViewById(R.id.reg_password);
+	    EditText et3 = (EditText)findViewById(R.id.reg_password2);
+	    EditText et4 = (EditText)findViewById(R.id.reg_email);
+	    EditText et5 = (EditText)findViewById(R.id.reg_name);
+	    EditText et6 = (EditText)findViewById(R.id.reg_surname);
+	    String userName = et1.getText().toString();
+	    String pass1 = et2.getText().toString();
+	    String pass2 = et3.getText().toString();
+	    String email = et4.getText().toString();
+	    String name = et5.getText().toString();
+	    String surname = et6.getText().toString();
+	    if(userName.equals("")){
+	    	Toast.makeText(getApplicationContext(), "Enter Username!",
+	    			Toast.LENGTH_LONG).show();
+	    } else if(pass1.equals("")){
+	    	Toast.makeText(getApplicationContext(), "Enter password!",
+	    			Toast.LENGTH_LONG).show();
+	    } else if(pass2.equals("")){
+	    	Toast.makeText(getApplicationContext(), "Enter password!",
+	    			Toast.LENGTH_LONG).show();
+	    } else if(email.equals("")){
+	    	Toast.makeText(getApplicationContext(), "Enter e-mail!",
+	    			Toast.LENGTH_LONG).show();
+	    } else if(!pass1.equals(pass2)){
+	    	Toast.makeText(getApplicationContext(), "Passwords are different!",
+	    			Toast.LENGTH_LONG).show();
+	    } else {
+	    	
+	    }
+	}
+	
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
