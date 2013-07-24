d48cf4c2be2919df9273ac811d58ab74ce8ad571
Kote Ujmajuridze <kujma10@freeuni.edu.ge>
2013-07-24 22:41:28 +0400

    glexuri logini gadaketda

--- src/com/example/myauto/requests/LoginRequest.java
+++ src/com/example/myauto/requests/LoginRequest.java
@@ -1,43 +1,36 @@
 package com.example.myauto.requests;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 /**
  * TODO Put here a description of what this class does.
- *
- * @author Kote.
- *         Created Jul 21, 2013.
+ * 
+ * @author Kote. Created Jul 21, 2013.
  */
 public class LoginRequest {
-	
+
 	private String userName;
 	private String password;
 	private boolean logined;
-	
-	public LoginRequest(String userName, String password){
+
+	public LoginRequest(String userName, String password) {
 		this.userName = userName;
 		this.password = password;
 		logined = false;
 	}
-	
-	public boolean sendLoginRequest(){
+
+	public boolean sendLoginRequest() {
 		Thread th = new Thread() {
 			@Override
-			public void run() {				
+			public void run() {
 				doPostR();
 			}
 		};
@@ -50,60 +43,28 @@ public class LoginRequest {
 		}
 		return logined;
 	}
-	
+
 	/**
 	 * TODO Put here a description of what this method does.
-	 *
+	 * 
 	 * @param userName
 	 * @param pass
 	 */
 	private void doPostR() {
 		HttpClient httpclient = new DefaultHttpClient();
-		String cookies = "__unam=656d566-13b320bb773-3236022d-117; PHPSESSID=o11blrta66tc0p5oqeboi3m792; ad_date_2=2013-07-24; __utma=229565184.325010435.1337247288.1374057018.1374064773.87; __utmb=229565184.2.10.1374064773; __utmc=229565184; __utmz=229565184.1373798325.83.22.utmcsr=forum.ge|utmccn=(referral)|utmcmd=referral|utmcct=/";
-		HttpPost httppost = new HttpPost("http://myauto.ge/login.php");
+		HttpGet httpget = new HttpGet(
+				"http://myauto.ge/android/login.php?username=" + userName
+						+ "&password=" + getMD5Hash("fc9" + password + "48c"));
 
 		try {
-			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-//			nameValuePairs.add(new BasicNameValuePair("ad_date_2", "2013-07-17"));
-			nameValuePairs.add(new BasicNameValuePair("action", "do_login"));
-			nameValuePairs.add(new BasicNameValuePair("password", password));
-			nameValuePairs.add(new BasicNameValuePair("username", userName));
-			//nameValuePairs.add(new BasicNameValuePair("PHPSESSID", "o11blrta66tc0p5oqeboi3m792"));
-			
-			
-			
-			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-			httppost.setHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-			httppost.setHeader("Accept-Encoding", "gzip,deflate,sdch");
-			httppost.setHeader("Accept-Language", "en-US,en;q=0.8");
-			httppost.setHeader("Cache-Control", "max-age=0");
-			httppost.setHeader("Connection", "keep-alive");
-			httppost.setHeader("Content-Type", "application/x-www-form-urlencoded");
-			httppost.setHeader("Cookie", cookies);
-			httppost.setHeader("Host", "www.myauto.ge");
-			httppost.setHeader("Origin", "http://www.myauto.ge");
-			httppost.setHeader("Referer", "http://www.myauto.ge/login.php");
-			
-			HttpResponse response = httpclient.execute(httppost);
+
+			HttpResponse response = httpclient.execute(httpget);
 			HttpEntity entity = response.getEntity();
 
 			String responseText = EntityUtils.toString(entity);
-			System.out.println("statuscode " + response.getStatusLine());
-			System.out.println("-----------------------------------------------");
-			if(responseText.contains("<a href=\"mypage.php\">" + userName + "</a>"))logined = true;
-			
-			HttpGet httpget = new HttpGet("http://www.myauto.ge/mypage.php");
-			nameValuePairs = new ArrayList<NameValuePair>(2);
-			nameValuePairs.add(new BasicNameValuePair("PHPSESSID", "o11blrta66tc0p5oqeboi3m792"));
-			httpget.setHeader("Cookie", cookies);
-			//httpget.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-			response = httpclient.execute(httpget);
-			entity = response.getEntity();
-
-			responseText = EntityUtils.toString(entity);
-			
-			System.out.println("code2!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! " + response.getStatusLine().getStatusCode());
-			System.out.println(responseText);
+			// System.out.println(responseText);
+			if (Integer.parseInt(responseText) == 0)
+				logined = true;
 
 		} catch (ClientProtocolException e) {
 			// TODO Auto-generated catch block
@@ -111,5 +72,28 @@ public class LoginRequest {
 			// TODO Auto-generated catch block
 		}
 	}
-	
+
+	/**
+	 * TODO Put here a description of what this method does.
+	 * 
+	 * @param password2
+	 * @return
+	 */
+	private String getMD5Hash(String pass) {
+		try {
+			java.security.MessageDigest md = java.security.MessageDigest
+					.getInstance("MD5");
+			byte[] array = md.digest(pass.getBytes());
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < array.length; ++i) {
+				sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100)
+						.substring(1, 3));
+			}
+			return sb.toString();
+		} catch (java.security.NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
 }
