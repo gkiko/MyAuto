6853cfe054a387ec55a2b493a0fa764a34bfe622
Giorgi Kikolashvili <gkiko10@freeuni.edu.ge>
2013-07-25 00:36:11 +0400

    bug gasworda

--- src/com/example/myauto/MainActivity.java
+++ src/com/example/myauto/MainActivity.java
@@ -91,8 +91,6 @@ public class MainActivity extends MasterPageActivity implements ImageDownloadLis
 		Bundle extras = getIntent().getExtras();
 		extras.putSerializable(FirstPageActivity.bundleKey, adapter.getList());
 		getIntent().putExtras(extras);
-		
-		DataContainer.setNewList(ls);
 	}
 	
 	
