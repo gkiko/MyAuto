bf2803fbb28df4e62a2076997735057b3a4e12d6
Kote Ujmajuridze <kujma10@gmail.com>
2013-09-06 16:28:20 +0400

    siroba

--- src/com/example/myauto/EditAccountActivity.java
+++ src/com/example/myauto/EditAccountActivity.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.apache.http.ParseException;
 import org.xmlpull.v1.XmlPullParserException;
 
+import com.example.myauto.database.DBManager;
 import com.example.myauto.item.Item;
 import com.example.myauto.net.Parser;
 import com.example.myauto.requests.UserAuthRequests;
@@ -16,21 +17,30 @@ import com.example.myauto.user.Profile;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TableLayout.LayoutParams;
 
 public class EditAccountActivity extends MasterPageActivity {
 
 	private Spinner years;
+	private SharedPreferences prefs;
+	private static final int LANG_EN = 1;
+	private static final int LANG_GE = 2;
+	private static final int LANG_RU = 3;
 
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_edit_account);
+		prefs = getSharedPreferences(
+				getResources().getString(R.string.shared_prefs), 0);
 		UserAuthRequests aur = UserAuthRequests.getInstance();
 		Profile myProfile = aur.getProfile();
 		System.out.println("aeeeeeeeeeeebu: " + myProfile.getValueFromProperty(Profile.USERNAME));
@@ -50,7 +60,8 @@ public class EditAccountActivity extends MasterPageActivity {
 		setYearsToSpinner(Integer.parseInt(pr.getValueFromProperty(Profile.BIRTH_YEAR)));
 		Spinner gender = (Spinner) findViewById(R.id.spinnerGender);
 		gender.setSelection(Integer.parseInt(pr.getValueFromProperty(Profile.GENDER_ID)) - 1);
-		Spinner locations = (Spinner) findViewById(R.id.spinnerLocation);
+		
+		setLocationsToSpinner(pr.getValueFromProperty(Profile.LOCATION_ID));
 		tv.setText(pr.getValueFromProperty(Profile.USERNAME));
 		et1.setText(pr.getValueFromProperty(Profile.NAME));
 		et2.setText(pr.getValueFromProperty(Profile.USER_SURNAME));
@@ -58,6 +69,38 @@ public class EditAccountActivity extends MasterPageActivity {
 		et4.setText(pr.getValueFromProperty(Profile.USER_LAST_PHONE));
 	}
 	
+	private void setLocationsToSpinner(String loc) {
+		Spinner locations = (Spinner) findViewById(R.id.spinnerLocation);
+		ArrayList<String[]> list = DBManager.getLocations();
+		String[] location;
+		for (int i = 0; i < list.size(); i++) {
+			location = list.get(i);
+			RadioButton rdbtn = new RadioButton(this);
+			rdbtn.setId(Integer.parseInt(location[0]));
+			int langID = prefs.getInt("Lang", LANG_EN);
+			switch (langID) {
+			case LANG_EN:
+				rdbtn.setText(location[LANG_EN + 1]);
+				break;
+			case LANG_GE:
+				rdbtn.setText(location[LANG_GE + 1]);
+				break;
+			case LANG_RU:
+				rdbtn.setText(location[LANG_RU + 1]);
+				break;
+			default:
+				break;
+			}
+			rdbtn.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT,
+					LayoutParams.WRAP_CONTENT));
+			//group.addView(rdbtn);
+		}
+//		ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<String>(this,
+//				android.R.layout.simple_spinner_dropdown_item, arr);
+//		locations.setAdapter(arrayAdapter1);
+//		locations.setSelection(arrayAdapter1.getPosition(loc));
+	}
+
 	/**
 	 * TODO Put here a description of what this method does.
 	 * 
