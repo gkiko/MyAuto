e958f6babd8c7afb7c11c172fa5eddfe8d045d59
Giorgi Kikolashvili <gkiko10@freeuni.edu.ge>
2013-09-12 16:55:27 +0400

    refactoring

--- src/com/example/myauto/requests/RegisterRequest.java
+++ src/com/example/myauto/requests/RegisterRequest.java
@@ -1,27 +1,15 @@
 package com.example.myauto.requests;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
-import org.apache.http.util.EntityUtils;
 
-/**
- * TODO Put here a description of what this class does.
- * 
- * @author Kote. Created Jul 21, 2013.
- */
 public class RegisterRequest {
-	String[] params;
+	private static final String userRegisterUrl = "http://www.myauto.ge/android/register.php";
+
+	private String[] params;
 	private int res = -1;
 
 	public RegisterRequest(String[] params) {
@@ -46,37 +34,32 @@ public class RegisterRequest {
 	}
 
 	private void register() {
-		HttpClient httpclient = new DefaultHttpClient();
-		HttpPost httppost = new HttpPost(
-				"http://www.myauto.ge/android/register.php");
+		Map<String, String> paramsAsMap = new HashMap<String, String>();
+		paramsAsMap.put("username", params[0]);
+		paramsAsMap.put("confirmpass", params[1]);
+		paramsAsMap.put("name", params[2]);
+		paramsAsMap.put("surname", params[3]);
+		paramsAsMap.put("email", params[4]);
+		paramsAsMap.put("birth_year", params[6]);
 
+		String responseText = doPostR(userRegisterUrl, paramsAsMap);
 		try {
-			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-			nameValuePairs.add(new BasicNameValuePair("password", params[1]));
-			nameValuePairs.add(new BasicNameValuePair("username", params[0]));
-			nameValuePairs.add(new BasicNameValuePair("confirmpass", params[1]));
-			nameValuePairs.add(new BasicNameValuePair("name", params[2]));
-			nameValuePairs.add(new BasicNameValuePair("surname", params[3]));
-			nameValuePairs.add(new BasicNameValuePair("email", params[4]));
-			nameValuePairs.add(new BasicNameValuePair("gender_id", params[5]));
-			nameValuePairs.add(new BasicNameValuePair("birth_year", params[6]));
-
-			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-
-			HttpResponse response = httpclient.execute(httppost);
-			HttpEntity entity = response.getEntity();
-
-			String responseText = EntityUtils.toString(entity);
-			try {
-				res = Integer.parseInt(responseText);
-			} catch (NumberFormatException e) {
-				res = -1;
-			}
-
-		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			res = Integer.parseInt(responseText);
+		} catch (NumberFormatException e) {
+			res = -1;
 		}
+	}
+
+	private String doPostR(String url, Map<String, String> params) {
+		String responseText = "";
+		try {
+			responseText = com.example.myauto.net.HttpClient
+					.getHttpClientDoGetResponse(url, params);
+		} catch (ClientProtocolException e1) {
+			e1.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return responseText;
 	}
 }
